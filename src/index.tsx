99a3b4b5c9b5ec967ced813756ac4a2df5774c20
Joe Kurtz <jwkurtz@mtu.edu>
2025-01-21 23:29:12 -0500

    Add gh pages to prepare for github.io deploy

--- src/index.tsx
+++ src/index.tsx
@@ -12,27 +12,27 @@ import { Root } from './Root.tsx';
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <Root />,
+        element: <TspChess />,
         errorElement: <ErrorPage />,
-        children: [
-            {
-                path: 'ConnectFour',
-                element: <ConnectFour />,
-            },
-            {
-                path: 'Chess',
-                element: <TspChess />,
-            },
-            {
-                path: 'WelcomePage',
-                element: <WelcomePage />,
-            },
-        ],
+        // children: [
+        //     {
+        //         path: 'ConnectFour',
+        //         element: <ConnectFour />,
+        //     },
+        //     {
+        //         path: 'Chess',
+        //         element: <TspChess />,
+        //     },
+        //     {
+        //         path: 'WelcomePage',
+        //         element: <WelcomePage />,
+        //     },
+        // ],
     },
 ]);
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-ReactDOM.createRoot(document.querySelector('#root')).render(
+ReactDOM.createRoot(document.querySelector('#root')!).render(
     <React.StrictMode>
         <RouterProvider router={router} />
     </React.StrictMode>
