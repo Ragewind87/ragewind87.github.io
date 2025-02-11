d5eafe2a67e317e3969674192e98f27b25daccec
Joe Kurtz <jwkurtz@mtu.edu>
2025-02-11 01:44:17 -0500

    One day I won't have to fiddle with the css constantly.

--- src/WelcomePage.tsx
+++ src/WelcomePage.tsx
@@ -6,29 +6,52 @@ import { sidePanelsColor } from './Root';
 
 const contactEmail = 'ragewind48@gmail.com';
 
-const footerStyle: React.CSSProperties = {};
+const footerStyle: React.CSSProperties = {
+    width: '100%',
+    padding: '15px',
+    textAlign: 'center',
+    borderTop: '1.5px solid black',
+    backgroundColor: 'rgb(50, 50, 50)',
+    color: 'white',
+    lineHeight: '16px',
+};
 
-const centerPanelStyle: React.CSSProperties = {};
+const cardStyle: React.CSSProperties = {
+    border: '2px solid black',
+    borderRadius: '7px',
+    alignContent: 'center',
+    marginTop: '15px',
+    backgroundColor: 'rgb(74, 74, 74)',
+    minWidth: '50.5vw',
+    maxWidth: '50.5vw',
+    minHeight: '91.5vh',
+    maxHeight: '91.5vh',
+    boxShadow: '10px 10px 15px black',
+    zIndex: 5,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    overflow: 'hidden',
+};
 
-const headingStyle: React.CSSProperties = {
-    fontSize: '4rem',
+const headerTextStyle: React.CSSProperties = {
+    display: 'flex',
+    textAlign: 'center',
+    flexDirection: 'column',
+    fontSize: '32px',
     fontWeight: 'bold',
-    marginBottom: '20px',
-    fontFamily: 'Arial, sans-serif',
-    marginTop: 30,
+    fontFamily: 'Calibri, sans-serif',
+    textShadow: '2px 2px 3px black',
+    padding: '20px',
 };
 
-const subheadingStyle: React.CSSProperties = {
-    fontSize: '2rem',
-    fontWeight: 'lighter',
-    marginBottom: '20px',
-    fontFamily: 'Arial, sans-serif',
-};
-
-const paragraphStyle: React.CSSProperties = {
-    fontSize: '1.5rem',
-    fontWeight: 'normal',
-    fontFamily: 'Arial, sans-serif',
+const imageStyle: React.CSSProperties = {
+    maxWidth: '100%',
+    maxHeight: '400px',
+    width: 'auto',
+    height: 'auto',
+    objectFit: 'contain',
+    padding: '20px',
 };
 
 export const WelcomePage: React.FunctionComponent = (properties) => {
@@ -44,73 +67,27 @@ export const WelcomePage: React.FunctionComponent = (properties) => {
             <div style={leftPanelStyle}></div>
 
             {/* Main Panel */}
-            <div
-                className="main"
-                style={{
-                    border: '2px solid black',
-                    alignContent: 'center',
-                    padding: '20px',
-                    marginTop: '15px',
-                    backgroundColor: 'rgb(74, 74, 74)',
-                    minWidth: '50.5vw',
-                    maxWidth: '50.5vw',
-                    minHeight: '91.5vh',
-                    maxHeight: '91.5vh',
-                    boxShadow: '10px 10px 15px black',
-                    zIndex: 5,
-                    position: 'relative',
-                }}
-            >
-                <div
-                    style={{
-                        flex: 1,
-                    }}
-                ></div>
-                <Stack style={{ display: 'flex', alignItems: 'center' }}>
-                    <img className="faint-glow" style={{ maxHeight: '400px' }} src={CordyTank} alt="Cordy Tank" />
-                    <div
-                        style={{
-                            display: 'flex', // Make this div a flex container
-                            alignItems: 'center', // Center items vertically
-                            flexDirection: 'column', // Align items in a column
-                            fontSize: '32px',
-                            fontWeight: 'bold',
-                            fontFamily: 'Calibri, sans-serif',
-                            marginTop: '30px',
-                            textShadow: '2px 2px 3px black',
-                        }}
-                    >
+            <div className="main" style={cardStyle}>
+                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <div style={headerTextStyle}>
                         <span>{'Welcome to Cordy Game Hub!'}</span>
                         <span>{'ONWARD!'}</span>
                     </div>
-                </Stack>
-                <div
-                    style={{
-                        position: 'absolute',
-                        bottom: 0,
-                        right: 0,
-                        height: 'fit-content',
-                        width: '100%',
-                        padding: '8px',
-                        textAlign: 'center',
-                        borderTop: '1.5px solid black',
-                        backgroundColor: 'rgb(50, 50, 50)',
-                        color: 'white',
-                    }}
-                >
+                    <img className="faint-glow" style={imageStyle} src={CordyTank} alt="Cordy Tank" />
+                </div>
+                <div style={footerStyle}>
                     <div>
-                        This app was built by{' '}
+                        This site was built by{' '}
                         <a
                             href="https://www.linkedin.com/in/jwkurtz44/"
                             style={{ color: 'lightblue', textDecoration: 'underline' }}
                         >
-                            Joe Kurtz
+                            Joseph Kurtz
                         </a>
-                        . I&apos;m a full stack developer with an interest in the front-end.
-                        <br />I enjoy working with React and this this is a personal project I'm building to help
-                        solidify these skills.
+                        . I&apos;m a full stack developer with an interest in the front-end. I enjoy working with React
+                        and this this is a personal project I'm building to help solidify these skills.
                     </div>
-                    <div style={{ marginTop: '15px' }}>
+                    <div style={{ marginTop: '25px' }}>
                         The art for this app created by a talented friend. Please direct any inquiries to: &nbsp;&nbsp;
                         <span>
                             <a
