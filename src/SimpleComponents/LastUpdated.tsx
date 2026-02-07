4fa7df5f4abbec253c04df8f6e97a4c5b78d9b47
Joe Kurtz <ragewind48@gmail.com>
2026-02-07 17:34:10 -0500

    last updated

--- src/SimpleComponents/LastUpdated.tsx
+++ src/SimpleComponents/LastUpdated.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 
-export const lastUpdatedString = `Last updated:\u00A0\u00A0\u00A02/04/2026 10:46pm`;
-
 const style = {
     fontSize: '12px',
 };
+
+const time = '2/07/2026 5:33pm';
+const lastUpdatedString = `Last updated:\u00A0\u00A0\u00A0${time}`;
 
 const LastUpdated: React.FC = () => {
     return <div style={style}>{lastUpdatedString}</div>;
